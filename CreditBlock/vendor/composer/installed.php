c30ea7538fedf7cd9197ed5a91291a134fd01e16
yohanpratamaa <yohanpratamaa@gmail.com>
2025-03-25 19:12:16 +0700

    update

--- CreditBlock/vendor/composer/installed.php
+++ CreditBlock/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v12.0.3',
-        'version' => '12.0.3.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'faad101f98cb053d23d41fabc744640e03942083',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -386,9 +386,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v12.0.3',
-            'version' => '12.0.3.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'faad101f98cb053d23d41fabc744640e03942083',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
